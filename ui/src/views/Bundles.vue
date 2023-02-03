e5cdafc1d3ad70b7fe78e66d20e57098b6db503f
raul <raul.cabello@suse.com>
2023-02-03 11:18:51 +0100

    add not implemented yet to views that are not available

--- ui/src/views/Bundles.vue
+++ ui/src/views/Bundles.vue
@@ -1,22 +1,8 @@
 <script setup>
-import { ref, onMounted } from 'vue';
-import axios from 'axios'
-
-const bundles = ref([])
-
-onMounted(() => {
-  axios.get('http://localhost:8080/bundles/fleet-default')
-      .then((response) => {
-        console.log(response.data)
-        bundles.value = response.data.items
-      })
-})
 </script>
 
 <template>
-  <main>
-
-  </main>
+    <p  class="text-center mt-4">Not implemented yet!</p>
 </template>
 
 <style>
--- ui/src/views/Clusters.vue
+++ ui/src/views/Clusters.vue
@@ -1,5 +1,5 @@
 <template>
-
+  <p  class="text-center mt-4">Not implemented yet!</p>
 </template>
 
 <style>
